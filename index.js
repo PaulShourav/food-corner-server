d68648ccad7e845ae9468d24283ee0022857c4ad
Shourav paul <paul.shourav01@gmail.com>
2023-05-06 15:31:08 +0600

    Recipe Data updated

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const chefRecipeData = require('./chrfRecipesData.json')
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world')
-});
+})
 app.get('/chefData', (req, res) => {
     res.send(chefData)
-  })
-  app.get('/chefRecipeData', (req, res) => {
+});
+app.get('/chefRecipeData', (req, res) => {
     res.send(chefRecipeData)
-  })
+})
 app.listen(5000,()=>{
     console.log("Server is running 5000");
 })
