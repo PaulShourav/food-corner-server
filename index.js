daf8304e25f856cfb65870955efb1fcc39fa5f49
Shourav paul <paul.shourav01@gmail.com>
2023-05-06 14:48:32 +0600

    Recipe Data

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 const chefData = require('./chefData.json')
+const chefRecipeData = require('./chrfRecipesData.json')
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world')
 });
 app.get('/chefData', (req, res) => {
     res.send(chefData)
+  })
+  app.get('/chefRecipeData', (req, res) => {
+    res.send(chefRecipeData)
   })
 app.listen(5000,()=>{
     console.log("Server is running 5000");
